5c1775a010d191f8f4adffcf3cf7129f3c842780
Gabriel <riellopez29@gmail.com>
2019-08-24 08:13:54 +0800

    first commit

---
+++ numbers/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    Absolute Value: abs
+    Exponential: pow(base,exp)
+    Square Root: sqrt(value)
+    Modulo: fmod(float,float)
+    Random: rand();
+    Random(min,max): rand(min,max)
+</body>
+</html>
